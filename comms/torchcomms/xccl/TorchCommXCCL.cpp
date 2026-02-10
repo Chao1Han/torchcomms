2c6ab6821949ab4527d5e953fb42e9f04717681b
lzhang2 <cherry.zhang@intel.com>
2026-02-10 10:45:56 -0800

    Fix XCCL backend building failure (#432)
    Summary:
    TorchComms refines some base classes, this breaks XCCL backend. Raise a PR to fix the XCCL backend building failure.
    
    Pull Request resolved: https://github.com/meta-pytorch/torchcomms/pull/432
    
    Reviewed By: siyengar
    
    Differential Revision: D92838622
    
    Pulled By: d4l3k
    
    fbshipit-source-id: 29f94ee70769fe979f5dd1cde561cda6d104bca8

--- comms/torchcomms/xccl/TorchCommXCCL.cpp
+++ comms/torchcomms/xccl/TorchCommXCCL.cpp
@@ -606,11 +606,6 @@ std::shared_ptr<TorchCommBackend> TorchCommXCCL::split(
   return new_torchcomm;
 }
 
-std::shared_ptr<c10::Allocator> TorchCommXCCL::getMemAllocator() {
-  throw std::runtime_error(
-      "XCCL getMemAllocator is not supported now and will be added later");
-}
-
 XCCLException::XCCLException(
     XcclApi& xccl_api,
     const std::string& message,
